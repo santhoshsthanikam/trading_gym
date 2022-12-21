3279592c2929c3ad9288adc7d4922a6b911f7a40
Santhosh S Kuumar <sakuumar@in.ibm.com>
2022-12-21 11:31:04 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import gym
+import json
+import datetime as dt
+
+# from stable_baselines3.common.policies import MlpPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3 import PPO
+import talib as ta
+from env.StockTradingEnv import StockTradingEnv
+
+import pandas as pd
+
+df = pd.read_csv('./data/AAPL.csv')
+df = df.sort_values('Date')
+df.dropna(inplace=True)
+df = df.sort_values('Date')
+df = df.reset_index()
+
+# The algorithms require a vectorized environment to run
+env = DummyVecEnv([lambda: StockTradingEnv(df)])
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=20000)
+
+obs = env.reset()
+for i in range(2000):
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    env.render()
